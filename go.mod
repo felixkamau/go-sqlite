374060c2e264d99cd1fa11c97ad4f62ad9bc2f2f
felixkamau <kamaufelix16@gmail.com>
2025-06-07 22:59:17 +0300

    Initialize SQLite database and create tasks table

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/felixkamau/go-sqlite
 
 go 1.22.4
 
-require github.com/mattn/go-sqlite3 v1.14.28 // indirect
+require github.com/mattn/go-sqlite3 v1.14.28
--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main(){
-	
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	//  Create a new db  and create a table in the db
+	db, err := sql.Open("sqlite3", "./tasks.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// defer close() ensures the resource is closed when main() finishes
+	defer db.Close()
+	// sql-statement to create a table
+	sqlStmt := `
+		CREATE TABLE IF NOT EXISTS tasks (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			task TEXT,
+			status BOOL
+		);
+	`
+	// Execute the sql-script
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// `sqlite3 tasks.db .schema` to check schema
+	log.Println("Table 'tasks' created successfully")
 }
