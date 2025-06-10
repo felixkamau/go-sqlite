986be0390fa39389c9449437d7ef5b57f541c5f7
felixkamau <kamaufelix16@gmail.com>
2025-06-10 15:25:24 +0300

    feat: data DELETE from db

--- main.go
+++ main.go
@@ -16,7 +16,20 @@ func main() {
 
 	// defer close() ensures the resource is closed when main() finishes
 	defer db.Close()
-	rows, err := db.Query("SELECT id, task, status FROM tasks")
+	id := 1
+	result, err := db.Exec("DELETE FROM tasks WHERE id = ?", id)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	affectedRows, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Deleted %d rows(s) \n", affectedRows)
+	// rows, err := db.Query("SELECT id, task, status FROM tasks")
 	// sql-statement to create a table
 	// sqlStmt := `
 	// 	CREATE TABLE IF NOT EXISTS tasks (
@@ -33,25 +46,25 @@ func main() {
 	// status := true // or false if not done
 	// _, err = db.Exec("INSERT INTO tasks(task, status) VALUES(?, ?)", task, status)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var task string
-		var status bool
-		err = rows.Scan(&id, &task, &status)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("Task: %d, %s, %v", id, task, status)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var id int
+	// 	var task string
+	// 	var status bool
+	// 	err = rows.Scan(&id, &task, &status)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	log.Printf("Task: %d, %s, %v", id, task, status)
 
-	}
+	// }
 
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
+	// if err = rows.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
 	// `sqlite3 tasks.db .schema` to check schema
 	// log.Println("New 'task' added successfully")
 
